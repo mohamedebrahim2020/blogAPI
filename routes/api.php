9bae6475ef6f9d0afc94d48dcf4bf29120b33a0e
himaaaintake40 <mohamedebrahimabbas@gmail.com>
2020-07-19 12:45:32 +0200

    final apis

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+
 
 //Author registration 
 Route::post('/register','UserController@register');
